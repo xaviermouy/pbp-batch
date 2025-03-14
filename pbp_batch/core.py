67944f92dd9a4c20fab75a6741b4535e11ac0581
Xavier Mouy <xavier.mouy@gmail.com>
2025-03-14 09:23:00 -0400

    start prefect implementation

--- pbp_batch/core.py
+++ pbp_batch/core.py
@@ -15,7 +15,7 @@ from pbp.plot_const import (
     DEFAULT_CMLIM,
 )
 from prefect import flow, task
-from prefect.server.schemas.schedules import IntervalSchedule
+#from prefect.server.schemas.schedules import IntervalSchedule
 from datetime import timedelta
 
 def format_path(path):
@@ -23,7 +23,7 @@ def format_path(path):
     # Convert to Windows-style path
     return str(p.as_posix()).replace('/', '\\') if os.name == 'nt' else str(p)
 
-@task(retries=5, retry_delay_seconds=20)
+@task(retries=5, retry_delay_seconds=20, name='Write temp. YAML file')
 def write_pbp_glabalAttributes_file(config):
     yaml_template = {
         "title": "",
@@ -72,7 +72,7 @@ def write_pbp_glabalAttributes_file(config):
 
     print(f"Generated YAML saved to: {output_file}")
 
-#@task(retries=5, retry_delay_seconds=20)
+@task(retries=5, retry_delay_seconds=5, name="Load YAML file")
 def load_yaml_file(yaml_file):
     #yaml_file = str(yaml_file)
     with open(yaml_file, "r") as file:
@@ -106,7 +106,7 @@ def load_yaml_file(yaml_file):
     config['pbp_job_agent']['ylim'] = tuple(map(float, config['pbp_job_agent']['ylim'].split()))
     return config
 
-#@task(retries=5, retry_delay_seconds=20)
+@task(retries=5, retry_delay_seconds=5, name="PBP MetaGen")
 def run_pbp_meta_gen(recorder=None,uri=None,output_dir=None,json_base_dir=None,xml_dir=None,start=None,end=None,prefix=None):
     args ={
         "recorder": recorder,
@@ -120,7 +120,7 @@ def run_pbp_meta_gen(recorder=None,uri=None,output_dir=None,json_base_dir=None,x
     }
     run_main_meta_generator(Namespace(**args))
 
-#@task(retries=5, retry_delay_seconds=20)
+@task(retries=5, retry_delay_seconds=10, name="PBP HMD Gen daily")
 def run_pbp_hmd_gen(json_base_dir=None,audio_base_dir=None,date=None,output_dir=None,prefix=None,sensitivity_uri=None,sensitivity_flat_value=None,voltage_multiplier=None,subset_to=None,global_attrs=None,variable_attrs=None):
     # Simulate command-line arguments
     args = {
@@ -161,7 +161,7 @@ def run_pbp_hmd_gen(json_base_dir=None,audio_base_dir=None,date=None,output_dir=
     # Call main function
     run_main_hmb_generator(Namespace(**args))
 
-#@task(retries=5, retry_delay_seconds=20)
+@task(retries=5, retry_delay_seconds=10, name="PBP HMD Gen deployment")
 def run_pbp_hmd_gen_batch(json_base_dir=None,audio_base_dir=None,start=None,end=None,output_dir=None,prefix=None,sensitivity_uri=None,sensitivity_flat_value=None,voltage_multiplier=None,subset_to=None,global_attrs=None,variable_attrs=None):
     # loop through each day of the deployment
     date_format = "%Y%m%d"
@@ -188,8 +188,14 @@ def run_pbp_hmd_gen_batch(json_base_dir=None,audio_base_dir=None,start=None,end=
         )
         start_date += delta
 
-#@task(retries=5, retry_delay_seconds=20)
+@task(retries=5, retry_delay_seconds=5, name="PBP daily plots")
 def run_pbp_main_plot(netcdf_dir,latlon=DEFAULT_LAT_LON_FOR_SOLPOS, title=f"'{DEFAULT_TITLE}'", ylim=DEFAULT_YLIM, cmlim=DEFAULT_CMLIM, dpi=DEFAULT_DPI, show=False, only_show=False, engine="h5netcdf"):
+    import matplotlib
+    import matplotlib.pyplot as plt
+    matplotlib.use('Agg')
+    plt.close('all')
+    plt.show(block=True)
+
     nc_files_path_list = list(Path(netcdf_dir).rglob("*.nc"))
     nc_files_str_list = [str(p) for p in nc_files_path_list]
     args = {
@@ -205,7 +211,7 @@ def run_pbp_main_plot(netcdf_dir,latlon=DEFAULT_LAT_LON_FOR_SOLPOS, title=f"'{DE
     }
     run_main_plot(Namespace(**args))
 
-#@flow(name='pbp-job')
+@flow(name='pbp-job')
 def submit_job(yaml_file: Path):
     # load config parameters from YAML file
     #yaml_file = r"C:\Users\xavier.mouy\Documents\Projects\2025_Galapagos\processing_outputs\WHOI_Galapagos_202305_Caseta\6478\pypam\META\globalAttributes_WHOI_Galapagos_202305_Caseta.yaml"
